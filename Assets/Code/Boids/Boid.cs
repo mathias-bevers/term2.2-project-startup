96ddebf46ad80c2d50485958e9c1bb2f7aa5fc42
Mathias Bevers <mathiasbevers@outlook.com>
2022-11-22 13:41:39 +0100

    added boids code project

---
+++ Assets/Code/Boids/Boid.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+
+namespace Code.Boids
+{
+	public class Boid : MonoBehaviour
+	{
+		[HideInInspector] public int numPerceivedFlockmates;
+		[HideInInspector] public Vector3 avgAvoidanceHeading;
+		[HideInInspector] public Vector3 avgFlockHeading;
+		[HideInInspector] public Vector3 centreOfFlockmates;
+		[HideInInspector] public Vector3 forward;
+
+		// State
+		[HideInInspector] public Vector3 position;
+
+		private BoidSettings settings;
+
+		// Cached
+		private Material material;
+		private Transform cachedTransform;
+		private Transform target;
+
+		// To update:
+		private Vector3 acceleration;
+		private Vector3 velocity;
+
+		private void Awake()
+		{
+			material = transform.GetComponentInChildren<MeshRenderer>().material;
+			cachedTransform = transform;
+		}
+
+		public void Initialize(BoidSettings settings, Transform target)
+		{
+			this.target = target;
+			this.settings = settings;
+
+			position = cachedTransform.position;
+			forward = cachedTransform.forward;
+
+			float startSpeed = (settings.minSpeed + settings.maxSpeed) / 2;
+			velocity = transform.forward * startSpeed;
+		}
+
+		public void SetColour(Color col)
+		{
+			if (material != null) { material.color = col; }
+		}
+
+		public void UpdateBoid()
+		{
+			Vector3 acceleration = Vector3.zero;
+
+			if (target != null)
+			{
+				Vector3 offsetToTarget = target.position - position;
+				acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
+			}
+
+			if (numPerceivedFlockmates != 0)
+			{
+				centreOfFlockmates /= numPerceivedFlockmates;
+
+				Vector3 offsetToFlockmatesCentre = centreOfFlockmates - position;
+
+				Vector3 alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
+				Vector3 cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
+				Vector3 seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight;
+
+				acceleration += alignmentForce;
+				acceleration += cohesionForce;
+				acceleration += seperationForce;
+			}
+
+			if (IsHeadingForCollision())
+			{
+				Vector3 collisionAvoidDir = ObstacleRays();
+				Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
+				acceleration += collisionAvoidForce;
+			}
+
+			velocity += acceleration * Time.deltaTime;
+			float speed = velocity.magnitude;
+			Vector3 dir = velocity / speed;
+			speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed);
+			velocity = dir * speed;
+
+			cachedTransform.position += velocity * Time.deltaTime;
+			cachedTransform.forward = dir;
+			position = cachedTransform.position;
+			forward = dir;
+		}
+
+		private bool IsHeadingForCollision() => Physics.SphereCast(position,
+			settings.boundsRadius,
+			forward,
+			out RaycastHit _,
+			settings.collisionAvoidDst,
+			settings.obstacleMask);
+
+		private Vector3 ObstacleRays()
+		{
+			Vector3[] rayDirections = BoidHelper.directions;
+
+			for (int i = 0; i < rayDirections.Length; i++)
+			{
+				Vector3 dir = cachedTransform.TransformDirection(rayDirections[i]);
+				Ray ray = new(position, dir);
+				if (!Physics.SphereCast(ray, settings.boundsRadius, settings.collisionAvoidDst, settings.obstacleMask)) { return dir; }
+			}
+
+			return forward;
+		}
+
+		private Vector3 SteerTowards(Vector3 vector)
+		{
+			Vector3 v = (vector.normalized * settings.maxSpeed) - velocity;
+			return Vector3.ClampMagnitude(v, settings.maxSteerForce);
+		}
+	}
+}
---
+++ Assets/Code/Boids/BoidManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Code.Boids;
+using UnityEngine;
+
+namespace Code.Boids
+{
+	public class BoidManager : MonoBehaviour
+	{
+		const int threadGroupSize = 1024;
+
+		public BoidSettings settings;
+		public ComputeShader compute;
+		Boid[] boids;
+
+		void Start()
+		{
+			boids = FindObjectsOfType<Boid>();
+			foreach (Boid b in boids) { b.Initialize(settings, null); }
+
+		}
+
+		void Update()
+		{
+			if (boids != null)
+			{
+
+				int numBoids = boids.Length;
+				var boidData = new BoidData[numBoids];
+
+				for (int i = 0; i < boids.Length; i++)
+				{
+					boidData[i].position = boids[i].position;
+					boidData[i].direction = boids[i].forward;
+				}
+
+				var boidBuffer = new ComputeBuffer(numBoids, BoidData.Size);
+				boidBuffer.SetData(boidData);
+
+				compute.SetBuffer(0, "boids", boidBuffer);
+				compute.SetInt("numBoids", boids.Length);
+				compute.SetFloat("viewRadius", settings.perceptionRadius);
+				compute.SetFloat("avoidRadius", settings.avoidanceRadius);
+
+				int threadGroups = Mathf.CeilToInt(numBoids / (float)threadGroupSize);
+				compute.Dispatch(0, threadGroups, 1, 1);
+
+				boidBuffer.GetData(boidData);
+
+				for (int i = 0; i < boids.Length; i++)
+				{
+					boids[i].avgFlockHeading = boidData[i].flockHeading;
+					boids[i].centreOfFlockmates = boidData[i].flockCenter;
+					boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
+					boids[i].numPerceivedFlockmates = boidData[i].flockMatesCount;
+
+					boids[i].UpdateBoid();
+				}
+
+				boidBuffer.Release();
+			}
+		}
+	}
+}
