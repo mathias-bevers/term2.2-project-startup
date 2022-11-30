1398cd570b49ca7cac87ccbc0a4ec58d5d631697
Mathias Bevers <mathiasbevers@outlook.com>
2022-11-30 15:44:05 +0100

    treasure opened ending basic animation
    - removed: TMPro package

--- Assets/Code/DeveloperShortcuts.cs
+++ Assets/Code/DeveloperShortcuts.cs
@@ -6,6 +6,8 @@ namespace Code.Editor
 {
     public class DeveloperShortcuts : MonoBehaviour
     {
+        [SerializeField] private GameObject endingPanel;
+
         private Survivor survivor;
         private InteractionHandler interactionHandler;
 
@@ -20,6 +22,15 @@ namespace Code.Editor
             if (Input.GetKeyUp(KeyCode.F1))
             {
                 interactionHandler.inventory.Drop<Key>(survivor.transform.position);
+            }
+
+            if (Input.GetKeyUp(KeyCode.F2))
+            {
+                //TODO: move this to the survivor, or game handler etc.
+                endingPanel.SetActive(true);
+                Animation animation = endingPanel.GetComponent<Animation>();
+                animation["EndingScreen"].wrapMode = WrapMode.Once;
+                animation.Play();
             }
         }
     }
