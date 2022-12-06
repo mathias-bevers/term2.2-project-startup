f6223ccbe6ae579649396b51057007e9bef72c1a
Amber Kortier <AmberKortier@gmail.com>
2022-12-06 15:22:26 +0100

    Any overlayers

--- Assets/Code/Handlers/UI/EndGameAnimation.cs
+++ Assets/Code/Handlers/UI/EndGameAnimation.cs
@@ -7,6 +7,12 @@ public class EndGameAnimation : MonoBehaviour
 {
     [SerializeField] Canvas[] canvasses;
 
+    [SerializeField] Image[] images;
+    [SerializeField] Text[] texts;
+
+    bool survivorsWin;
+    float timer = 0;
+
     void Start()
     {
         for(int i = 0; i < canvasses.Length; i++)
@@ -15,8 +21,35 @@ public class EndGameAnimation : MonoBehaviour
         }
     }
 
+    public void SetWin(bool survivorsWin)
+    {
+        this.survivorsWin = survivorsWin;
+        gameObject.SetActive(true);
+    }
+
     void Update()
     {
-        
+        for (int i = 0; i < canvasses.Length; i++)
+        {
+            canvasses[i].enabled = true;
+        }
+        timer += Time.deltaTime;
+        for(int i = 0; i < images.Length; i++)
+        {
+            images[i].fillAmount = Mathf.Clamp01(Utils.Map(timer, 0, 2, 0, 1));
+        }
+        if(timer >= 2)
+        {
+            for (int i = 0; i < texts.Length; i++)
+            {
+                texts[i].text = (survivorsWin ? "The survivors Won!" : "The killer Won!");
+
+            }
+        }
+
+        if(timer >= 5)
+        {
+            Destroy(gameObject);
+        }
     }
 }
